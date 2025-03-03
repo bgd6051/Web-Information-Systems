c865e6dcbeed655f2f252e5131cea8338ddceda3
Diego Suarez <suarez.153960@e.unavarra.es>
2025-03-03 18:56:22 +0100

    Added Insert functions and DeleteAll function

--- src/DBDeletor.php
+++ src/DBDeletor.php
@@ -1,8 +1,23 @@
 <?php
 
 class DBDeletor extends DBHandler {
+    /*
     public function deleteUser($id) {
         $query = "DELETE FROM users WHERE id = ?";
         return $this->executeQuery($query, ["i", $id]);
+    }*/
+    public function deleteAll() {
+        $query = "DELETE FROM FINAL_ADMIN_LOG";
+        $this->executeQuery($query);
+        $query = "DELETE FROM FINAL_EXCHANGE_RATE";
+        $this->executeQuery($query);
+        $query = "DELETE FROM FINAL_SUPPORTED_COINS";
+        $this->executeQuery($query);
+        $query = "DELETE FROM FINAL_TRENDING_COINS";
+        $this->executeQuery($query);
+        $query = "DELETE FROM FINAL_COINS";
+        $this->executeQuery($query);
+        $query = "DELETE FROM FINAL_USER_REGISTRATION";
+        $this->executeQuery($query);
     }
 }
--- src/DBInsertor.php
+++ src/DBInsertor.php
@@ -1,7 +1,27 @@
 <?php
 class DBInsertor extends DBHandler {
-    public function insertUser($name, $email) {
-        $query = "INSERT INTO users (name, email) VALUES (?, ?)";
-        return $this->executeQuery($query, ["ss", $name, $email]);
+    public function insertCoin($coin) {
+        $query = "INSERT INTO FINAL_COINS (id, symbol, name) VALUES (?, ?, ?)";
+        return $this->executeQuery($query, ["ss", $coin->getId(), $coin->getSymbol(), $coin->getName()]);
+    }
+    public function insertAdminLog($adminLog) {
+        $query = "INSERT INTO FINAL_ADMIN_LOG (ID_ADMIN, action, fecha) VALUES (?, ?, ?)";
+        return $this->executeQuery($query, ["ss", $adminLog->getID_ADMIN(), $adminLog->getAction(), $adminLog->getFecha()]);
+    }
+    public function insertExchangeRate($exchangeRate) {
+        $query = "INSERT INTO FINAL_EXCHANGE_RATE (ID_COIN_FORM, ID_COIN_TO, date, price) VALUES (?, ?, ?, ?)";
+        return $this->executeQuery($query, ["ss", $exchangeRate->getID_COIN_FORM(), $exchangeRate->getID_COIN_TO(), $exchangeRate->getDate(), $exchangeRate->getPrice()]);
+    }
+    public function insertSupportedCoin($supportedCoin) {
+        $query = "INSERT INTO FINAL_SUPPORTED_COINS (ID_COIN) VALUES (?)";
+        return $this->executeQuery($query, ["ss", $supportedCoin->getID_COIN()]);
+    }
+    public function insertTrendingCoin($trendingCoin) {
+        $query = "INSERT INTO FINAL_TRENDING_COINS (ID_COIN, date) VALUES (?, ?)";
+        return $this->executeQuery($query, ["ss", $trendingCoin->getID_COIN(), $trendingCoin->getDate()]);
+    }
+    public function insertUserRegistration($userRegistration) {
+        $query = "INSERT INTO FINAL_USER_REGISTRATION (username, password, role) VALUES (?, ?, ?)";
+        return $this->executeQuery($query, ["ss", $userRegistration->getUsername(), $userRegistration->getPassword(), $userRegistration->getRole()]);
     }
 }
