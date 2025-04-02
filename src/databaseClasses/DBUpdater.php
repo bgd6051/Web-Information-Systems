c90f5df800748fac7f188ccf90d9bce42539a56d
Diego Suarez <suarez.153960@e.unavarra.es>
2025-04-02 19:19:18 +0200

    fixed missing SQL statement comma on updateUserRegistration

--- src/databaseClasses/DBUpdater.php
+++ src/databaseClasses/DBUpdater.php
@@ -2,7 +2,7 @@
 
 class DBUpdater extends DBHandler{
     public function updateUserRegistration(UserRegistration $userRegistration): bool {
-        $query = "UPDATE FINAL_USER_REGISTRATION SET username = ? password = ?, 
+        $query = "UPDATE FINAL_USER_REGISTRATION SET username = ?, password = ?, 
             role = ? WHERE ID_USER = ?";
         return $this->executeQuery($query,
          ["sssi", $userRegistration->getUsername(), 
