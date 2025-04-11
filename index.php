781cb01f61b3d60fa043e23f693004cd8eeca760
Bogdan <razvant.153957@e.unavarra.es>
2025-04-11 17:18:53 +0200

    Redesigned the main page content for when the user is logged, 'vista resumida' design completed

--- index.php
+++ index.php
@@ -2,19 +2,20 @@
 
 session_start();
 
-CONST MAIN_PAGE_TEMPLATE_PATH = "./web/templates/mainPage/";
+const MAIN_PAGE_TEMPLATE_PATH = "./web/templates/mainPage/";
 
 if (isset($_SESSION["Role"])) {
-    if(isset($_SESSION["Role"]) && $_SESSION["Role"] == "ADMIN") {
+    if (isset($_SESSION["Role"]) && $_SESSION["Role"] == "ADMIN") {
         $headAndHeader = file_get_contents(MAIN_PAGE_TEMPLATE_PATH . "adminHeadAndHeaderMainPage.html");
+        $content = file_get_contents(MAIN_PAGE_TEMPLATE_PATH . "adminContentMainPage.html");
     } else {
         $headAndHeader = file_get_contents(MAIN_PAGE_TEMPLATE_PATH . "registeredHeadAndHeaderMainPage.html");
+        $content = file_get_contents(MAIN_PAGE_TEMPLATE_PATH . "registeredContentMainPage.html");
     }
 } else {
     $headAndHeader = file_get_contents(MAIN_PAGE_TEMPLATE_PATH . "unloggedHeadAndHeaderMainPage.html");
+    $content = file_get_contents(MAIN_PAGE_TEMPLATE_PATH . "unloggedContentMainPage.html");
 }
-
-$content = file_get_contents(MAIN_PAGE_TEMPLATE_PATH . "contentMainPage.html");
 
 $footerAndScripts = file_get_contents(MAIN_PAGE_TEMPLATE_PATH . "footerAndScriptsMainPage.html");
 
