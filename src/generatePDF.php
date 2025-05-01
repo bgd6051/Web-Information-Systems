257052383b9a0482b05b073d3f5dfd649f5bf572
Bogdan <razvant.153957@e.unavarra.es>
2025-05-01 11:45:32 +0200

    Added autoload of php to php rquirements

--- src/generatePDF.php
+++ src/generatePDF.php
@@ -13,7 +13,7 @@ spl_autoload_register(function ($class) {
     }
 });
 
-//require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . '/vendor/autoload.php';//??
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . '/vendor/autoload.php';
 
 session_start();
 
